a948c111d7054e3daf79b05520c8212b29d97df4
Po-Chien Hao <pochienh@tw.ibm.com>
2021-07-31 17:44:11 +0800

    Initial practice of Posts controller & model with pages

---
+++ ror-project/blog/app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+    def index
+        puts ">>>>>> Posts Controller index!"
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        puts ">>>>>> Posts Controller new!"
+    end
+
+    def create
+        puts ">>>>>> Posts Controller create!"
+        # render json: params[:post]
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
+    end
+
+    private
+        # Prevent mass-assign
+        def post_params
+            params.require(:post).permit(:title, :text)
+        end
+end
